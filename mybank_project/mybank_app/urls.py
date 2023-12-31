1fe4c3894c5793a84ed43d983dbb9f9ab6f5fa75
lakshyatech <kanha.garg.jaipur@gmail.com>
2023-12-31 14:00:08 +0530

    data manipulation

--- mybank_project/mybank_app/urls.py
+++ mybank_project/mybank_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home,create_account
+from .views import home, create_account, manage_accounts, edit_account, delete_account
 
 urlpatterns = [
     path('', home, name='home'),
     path('create_account/', create_account, name='create_account'),
+    path('manage_accounts/', manage_accounts, name='manage_accounts'),
+    path('edit_account/<int:account_id>/', edit_account, name='edit_account'),
+    path('delete_account/<int:account_id>/', delete_account, name='delete_account'),
 ]
--- mybank_project/mybank_app/views.py
+++ mybank_project/mybank_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
-from .models import Service
 from .forms import BankAccountForm
+from .models import Service, BankAccount
+from django.shortcuts import render, redirect, get_object_or_404
 
 
 def home(request):
@@ -9,12 +9,54 @@ def home(request):
 
 
 def create_account(request):
+    # if request.method == 'POST':
+    #     form = BankAccountForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('home')  # Redirect to home page after successful submission
+    # else:
+    #     form = BankAccountForm()
+
+    if request.method == "POST":
+        full_name = request.POST['fullName']
+        phone_number = request.POST['phoneNumber']
+        residential_address = request.POST['residentialAddress']
+        citizenship = request.POST['citizenship']
+        value = BankAccount(
+            full_name=full_name,
+            phone_number=phone_number,
+            residential_address=residential_address,
+            citizenship=citizenship
+        )
+        value.save()
+        return redirect('home')
+    return render(request, 'create_account.html')
+
+
+def manage_accounts(request):
+    bank_accounts = BankAccount.objects.all()
+    return render(request, 'manage_accounts.html', {'bank_accounts': bank_accounts})
+
+
+def edit_account(request, account_id):
+    account = get_object_or_404(BankAccount, id=account_id)
+
     if request.method == 'POST':
-        form = BankAccountForm(request.POST, request.FILES)
+        form = BankAccountForm(request.POST, request.FILES, instance=account)
         if form.is_valid():
             form.save()
-            return redirect('home')  # Redirect to home page after successful submission
+            return redirect('manage_accounts')
     else:
-        form = BankAccountForm()
+        form = BankAccountForm(instance=account)
 
-    return render(request, 'create_account.html', {'form': form})
+    return render(request, 'edit_account.html', {'form': form, 'account': account})
+
+
+def delete_account(request, account_id):
+    account = get_object_or_404(BankAccount, id=account_id)
+
+    if request.method == 'POST':
+        account.delete()
+        return redirect('manage_accounts')
+
+    return render(request, 'delete_account.html', {'account': account})
